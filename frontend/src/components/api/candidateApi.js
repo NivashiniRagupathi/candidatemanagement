d7a1f73116ed8fc15e761753908a9cac53dafa07
NivashiniRagupathi <nivashiniragupathi@gmail.com>
2025-04-13 12:39:00 +0530

    Candidates

--- frontend/src/components/api/candidateApi.js
+++ frontend/src/components/api/candidateApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/candidates';
+const API_URL = 'https://candidatemanagement.onrender.com/api/candidates';
 
 export const getCandidates = () => axios.get(API_URL);
 export const addCandidate = (data) => axios.post(API_URL, data);
